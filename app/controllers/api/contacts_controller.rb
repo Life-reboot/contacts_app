2caa0e344c9c35b5faaf58eb2ab153228e65f7eb
Peter Jang <peter.x.jang@gmail.com>
2020-03-08 09:34:35 -0500

    Change all contacts action to RESTful contacts index action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,11 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
+  end
+
   def one_contact_action
     @contact = Contact.first
     render "one_contact.json.jb"
-  end
-
-  def all_contacts_action
-    @contacts = Contact.all
-    render "all_contacts.json.jb"
   end
 end
