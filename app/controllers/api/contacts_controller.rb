7a4a37b4c51a128ef47a736ec70640b4df0c1d43
Peter Jang <peter.x.jang@gmail.com>
2020-03-08 09:39:00 -0500

    Add RESTful contacts create action

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -4,6 +4,17 @@ class Api::ContactsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
   def show
     @contact = Contact.find_by(id: params[:id])
     render "show.json.jb"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
     get "/contacts/:id" => "contacts#show"
   end
 end
